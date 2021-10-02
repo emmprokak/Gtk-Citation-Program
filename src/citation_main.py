62e1cf6a085b446d3600b3273df33ed81e9ca45d
emmprokak <89413115+emmprokak@users.noreply.github.com>
2021-10-02 15:43:03 +0300

    Add files via upload

---
+++ src/citation_main.py
@@ -0,0 +1,31 @@
+"""
+Gtk Citation Program
+Copyright (C) 2021 Prokakis Emmanouil
+
+Gtk Citation Program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Gtk Citation Program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Gtk Citation Program. If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+
+from gi.repository import Gtk
+from init import GtkCitationProgram
+
+if __name__ == '__main__':
+	window = GtkCitationProgram()
+	window.connect("destroy", Gtk.main_quit)
+	window.show_all()
+	Gtk.main()
